e0f2fc0df8ab15b1ce477fb3e2571f3b1d4fba76
Astrid Lauenstein <me@acqrel.com>
2025-11-14 21:29:24 +0100

    Great Game

--- make.py
+++ make.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 
+
+def main():
+    with open("src/game.html", "r", encoding="utf8") as f:
+        game = f.read()
+
+    with open("index.html", "w", encoding="utf8") as f:
+        f.write(game)
+
+
+if __name__ == "__main__":
+    main()
