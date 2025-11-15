ac0103716620cdcd98870916a3503e457dc16231
Astrid Lauenstein <me@acqrel.com>
2025-11-15 11:24:58 +0100

    Maze generation

--- make.py
+++ make.py
@@ -2,8 +2,19 @@
 
 
 def main():
+    with open("src/maze.txt", "r", encoding="utf8") as f:
+        maze_lines = f.read()
+
+    maze_cells = ""
+    for y, line in enumerate(maze_lines.split("\n")):
+        for x, cell in enumerate(line.strip()):
+            if cell == ".":
+                maze_cells += f"<div class='cell' style='grid-area: {y+1}/{x+1}'></div>"
+
     with open("src/game.html", "r", encoding="utf8") as f:
         game = f.read()
+
+    game = game.replace("MAZE", maze_cells)
 
     with open("index.html", "w", encoding="utf8") as f:
         f.write(game)
